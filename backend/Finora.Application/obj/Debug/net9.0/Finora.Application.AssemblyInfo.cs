dfe476df74c348e7b660207b5c2fd636d3ba5c57
usuu03 <usunobu.omijie@gmail.com>
2025-07-28 23:17:42 +0100

    Update: - Testing upsert by adding and updating DB entries

--- backend/Finora.Application/obj/Debug/net9.0/Finora.Application.AssemblyInfo.cs
+++ backend/Finora.Application/obj/Debug/net9.0/Finora.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finora.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e293481045b98905e498543be6e3c499eb627326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a25e5557b63a10aa8be3aad3ab2084165b60e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finora.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finora.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
