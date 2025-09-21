8fdde9ba98c7641230658bc007d5cbc3101547f9
usuu03 <usunobu.omijie@gmail.com>
2025-09-21 19:48:35 +0100

    chore: Update assembly informational version and cache inputs

--- backend/Finora.WebApi/obj/Debug/net9.0/Finora.WebApi.AssemblyInfo.cs
+++ backend/Finora.WebApi/obj/Debug/net9.0/Finora.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finora.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39391fb9049bce9a2c5b8b6b5f7b97bee312d08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eaf19b7919cec3425e8edbde0eed48975e365e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finora.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finora.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
